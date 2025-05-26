ae41c68cc275acee29b87ca0e486ffb0d09757a8
SitanHuang <978494543.qq54@gmail.com>
2025-05-26 11:16:37 -0700

    ledg2 fish aliases

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -9,9 +9,13 @@ bass source ~/.bash_profile
 alias ll="ls -lh"
 
 alias lr="l --real"
-alias lra="l --real 'ass*|lia*' --hz --sum --tree=0"
-alias la="l 'ass*|lia*' --hz --tree=0 --sum"
+#alias lra="l --real 'ass*|lia*' --hz --sum --tree=0"
+alias la="ledg2 acc --hz --sp=false --tree=false -a '{asset,liability}*' --sum"
 alias lpend="l info pending:true"
+
+alias lra="ledg2 acc --hz --real --sp=false --tree=false -a '{asset,liability}*' --sum"
+
+alias lburn='l query " --name Income -a {income}* --invert" " --name Expense -a {expense}*" --monthly -f "beginning of last year" -t "beginning of next month" --currency=\$ --vs txnDate --plot'
 
 function lb
   l bs f:@min t:@max --interval=10000,0,0 --isofull --asset=\* $argv | \
